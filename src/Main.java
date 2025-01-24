09e24c3a8ed1f82ebe11762578d69c15ec9fe828
Priyanka <2024.priyanka.sharma@ves.ac.in>
2025-01-24 10:25:57 +0530

    hey commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        while (true) {
+
+
+            Scanner input = new Scanner(System.in);
+            System.out.println("enter sleep hrs");
+            int hours = input.nextInt();
+
+            if (hours < 0) {
+                System.out.println("sleep cant be -ve");
+                break;
+            }
+            int count = 1;
+            while (count <= 10) {
+                System.out.println(count * 5);
+                count++;
+            }
+        }
+    }
+}
